d945615bf009296ef09d402dd0cd6fdd0baac160
Razr-9 <runze_n@icloud.com>
2019-05-21 22:41:19 +1000

    prolog_proj2

--- Prolog-Programming/assignment2.pl
+++ Prolog-Programming/assignment2.pl
@@ -1,94 +1,76 @@
-correspond(X,[X|_],Y,[Y|_]).
-correspond(X, [_|TX], Y, [_|TY]) :- 
-correspond(X, TX, Y, TY).
+correspond(X, [X|_], Y, [Y|_]).
+correspond(X, [_|TX], Y, [_|TY]) :-
+    correspond(X, TX, Y, TY).
 
-interleave(A,B) :- 
-interleave_(A,B), length_same(A).
+interleave(A, B) :-
+    interleave_(A, B),
+    length_same(A).
 
-interleave_([],[]).
-interleave_([[]|T],[]) :- 
-interleave_(T,[]).
-interleave_([[H|T]|Ls], [H|L]) :- 
-append(Ls, [T], Lsn), interleave_(Lsn, L).
+interleave_([], []).
+interleave_([[]|T], []) :-
+    interleave_(T, []).
+interleave_([[H|T]|Ls], [H|L]) :-
+    append(Ls, [T], Lsn),
+    interleave_(Lsn, L).
 
-length_same([_|[]]).
-length_same([X1,X2|Xs]) :- 
-(
-  same_length(X1,X2) -> 
-  length_same([X2|Xs])
-).
-
-partial_eval(Expr0, Var, Val, Expr) :-
-  terms_to_list(Expr0,R1),
-  replace(R1,Var,Val,R2),
-  calculate(R2,Expr).
-
-terms_to_list(Terms,R) :-
-    (
-      compound(Terms) -> 
-      Terms=..[H,F,S],
-      (
-        compound(F) ->
-        terms_to_list(F,R1)
-        ;
-        R1 = F
-      ),
-      (
-        compound(S) ->
-        terms_to_list(S,R2)
-        ;
-        R2 = S
-      ),
-      append([H],[R1],Temp),
-      append(Temp,[R2],R)
-      ;
-      R = Terms
+length_same([_]).
+length_same([X1, X2|Xs]) :-
+    (   same_length(X1, X2)
+    ->  length_same([X2|Xs])
     ).
 
-replace([H,F,S],Var,Value,R) :-
-    (
-      is_list(F) -> 
-      replace(F,Var,Value,R1)
-      ;
-      (
-        atom(F), F = Var -> 
-        R1 = Value
-        ;
-        R1 = F
-      )
-    ),
-    (
-      is_list(S) -> 
-      replace(S,Var,Value,R2)
-      ;
-      (
-        atom(S), S = Var -> 
-        R2 = Value
-        ;
-        R2 = S
-      )
-    ),
-    append([H],[R1],Temp),
-    append(Temp,[R2],R).
+partial_eval(Expr0, Var, Val, Expr) :-
+    terms_to_list(Expr0, R1),
+    replace(R1, Var, Val, R2),
+    calculate(R2, Expr).
 
-calculate([H,F,S],R):-
-    (
-      is_list(F) -> 
-      calculate(F,R1)
-      ;
-      R1 = F
+terms_to_list(Terms, R) :-
+    (   compound(Terms)
+    ->  Terms=..[H, F, S],
+        (   compound(F)
+        ->  terms_to_list(F, R1)
+        ;   R1=F
+        ),
+        (   compound(S)
+        ->  terms_to_list(S, R2)
+        ;   R2=S
+        ),
+        append([H], [R1], Temp),
+        append(Temp, [R2], R)
+    ;   R=Terms
+    ).
+
+replace([H, F, S], Var, Value, R) :-
+    (   is_list(F)
+    ->  replace(F, Var, Value, R1)
+    ;   atom(F),
+        F=Var
+    ->  R1=Value
+    ;   R1=F
     ),
-    (
-      is_list(S) -> 
-      calculate(S,R2)
-      ;
-      R2 = S
+    (   is_list(S)
+    ->  replace(S, Var, Value, R2)
+    ;   atom(S),
+        S=Var
+    ->  R2=Value
+    ;   R2=S
     ),
-    E =..[H,R1,R2],
-    (
-      number(R1),number(R2) -> 
-      R is E
-      ;
-      R = E
+    append([H], [R1], Temp),
+    append(Temp, [R2], R).
+
+calculate([H, F, S], R) :-
+    (   is_list(F)
+    ->  calculate(F, R1)
+    ;   R1=F
+    ),
+    (   is_list(S)
+    ->  calculate(S, R2)
+    ;   R2=S
+    ),
+    E=..[H, R1, R2],
+    (   number(R1),
+        number(R2)
+    ->  R is E
+    ;   R=E
     ).
         
---
+++ Prolog-Programming/project2/proj2.pl
@@ -0,0 +1,152 @@
+% COMP90048 Declarative Programming
+% File: Project 2 
+% Author: Runze Nie 
+% LoginID: runzen 
+% StudentID: 956690
+
+% ------------------------------------------------------------------------------
+
+/* 
+Introduction: 
+
+This program is to solve a maths puzzle which 
+is a square grid of squares, each to be filled in with a single 
+digit 1-9.
+
+The programs using a main function puzzle_solution/1 to solve the  
+problem which includes three main steps in total. Including check 
+if the matrix is valid, then handle the diagonales, check if the 
+solutions conform to the rules and assign them.
+*/
+
+% for using function transpose/2
+:- use_module(library(clpfd)).
+
+/* 
+puzzle_solution/1,
+
+The main function of this program to solve the puzzles.
+
+Firstly, takes a list of lists named Puzzles as input and 
+get columns with transpose/2, then make sure the length 
+of rows is equal to the length of columns and each row 
+in the puzzles has the same length. 
+
+After, seprate the solutions from the puzzles and find 
+the diagonales then make sure they are all same.
+
+Finally, take all rows and columns to be solved together 
+and check them with the rules, including check if they are  
+valid and their sums or products are equal to the headings 
+and assign the values to each elements in Puzzles.
+*/
+puzzle_solution(Puzzles) :-
+    transpose(Puzzles,Columns),
+    length(Puzzles, LenPuzzles),
+    length(Columns, LenColumns),
+    Puzzles=[_|TailPuzzles],
+    LenPuzzles #= LenColumns,
+    maplist(same_length(Puzzles), Puzzles),
+    headings(TailPuzzles, Solutions),
+    diagonales(Solutions, Diagonales),
+    Diagonales ins 1..9,
+    all_same(Diagonales),
+    Puzzles=[_|TailPuzzles],
+    Columns=[_|TailColumns],
+    append(TailPuzzles,TailColumns,AllSituations),
+    all_valid(AllSituations),
+    handle_puzzles(AllSituations),
+    maplist(label,Puzzles).
+
+/* 
+headings/2,
+
+A function to seprate the Solutions part out of the Puzzles
+with recursions.
+
+The first element is a list of lists means Puzzles without the 
+first row, and the second one is a list means Solutions part.
+*/
+headings([], []).
+headings([[_|T]|Tail], [T|Tss]) :-
+    headings(Tail, Tss).
+
+/* 
+all_valid/1,
+
+A function to check if the Solutions (except the heading) are all 
+valid.
+
+The element means each row or column, constitutes headings and 
+solutions.
+
+Including check if it is a single number in 1 to 9 and if they 
+are all distinct or not. 
+*/
+all_valid([]).
+all_valid([H|T]) :-
+    H = [_|Tail],
+    Tail ins 1..9,
+    all_distinct(Tail),
+    all_valid(T).
+
+/* 
+handle_puzzles/1,
+
+A function to solve the puzzles with checking if the sum or 
+product of a column or a row is equal to the heading. 
+
+The element is a list of rows and columns, traversing the list 
+with recursions.
+*/
+handle_puzzles([]).
+handle_puzzles([Situaion|Others]) :-
+    Situaion = [Head|Solutions],
+    (
+        sum(Solutions,#=,Head);
+        product(Solutions,Head)
+    ),
+    handle_puzzles(Others).
+
+/* 
+all_same/1,
+
+A function to make sure all diagonales are same. 
+
+The element will be a list of numbers from diagonales/2
+*/
+all_same([_]).
+all_same([X1, X2|Xs]) :-
+    (   X1=X2
+    ->  all_same([X2|Xs])
+    ).
+
+/* 
+diagonales/2,
+
+A function to find the diagonales of a matrix.  
+
+The first element means the matrix and the second one means 
+a list of diagonales.
+*/
+diagonales([], []).
+diagonales([[E|_]|Ess], [E|Ds]) :-
+    maplist(list_tail, Ess, Ess0),
+    diagonales(Ess0, Ds).
+
+% get the tail of a list 
+list_tail([_|Tail], Tail).
+
+/* 
+product/2, 
+
+The function to calculate the product with all elements of a list.
+
+The first element is the list which can mean a row or a column, 
+the second one is a number means the product.
+*/
+product([],1):-!.
+product([Head|Tail],X):-
+    product(Tail,Y),
+    X #= Head * Y.
+
